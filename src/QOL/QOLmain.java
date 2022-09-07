ebfa536113ca0b2f503403c449727b7c27c8c093
Shidoengie <54602340+Shidoengie@users.noreply.github.com>
2022-09-07 11:19:04 +0100

    turip

--- src/QOL/QOLmain.java
+++ src/QOL/QOLmain.java
@@ -1,17 +1,16 @@
-package example;
+package QOL;
 
 import arc.*;
 import arc.util.*;
 import mindustry.*;
 import mindustry.content.*;
 import mindustry.game.EventType.*;
-import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class QOLmain extends Mod{
 
-    public ExampleJavaMod(){
+    public QOLmain(){
         Log.info("Loaded ExampleJavaMod constructor.");
 
         //listen for game load event
@@ -21,7 +20,7 @@ public class ExampleJavaMod extends Mod{
                 BaseDialog dialog = new BaseDialog("frog");
                 dialog.cont.add("behold").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+                dialog.cont.image(Core.atlas.find("QOLmain-frog")).pad(20f).row();
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
